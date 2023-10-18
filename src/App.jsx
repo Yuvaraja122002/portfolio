9ec19c5c995b58118ea60c0c16b005d031b77cac
Yuvaraja <paulyuvi02222@gmail.com>
2023-10-18 22:04:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/home/home'
+
+function App() {
+
+  return (
+   <main className="main">
+    <Home />
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/home/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import profileImg from "../../assets/profile-img.png";
+import shapeOne from "../../assets/shape-1.png";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
