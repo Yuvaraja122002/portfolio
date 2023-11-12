d721a963d94380e8e3599cfbc6de4e196871d12d
Yuvaraja <paulyuvi02222@gmail.com>
2023-11-12 18:21:52 +0530

    Skills finish - portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './components/home/home'
+import React from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/home/home";
+import Skills from "./components/Skills/Skills";
+import Portfolio from "./components/portfolio/portfolio";
 
 function App() {
-
   return (
-   <main className="main">
-    <Home />
-   </main>
-  )
+    <main className="main">
+      <Home />
+      <Skills />
+      <Portfolio />
+    </main>
+  );
 }
 
-export default App
+export default App;
