639fbe4a7c58928bc3d689681616cf87d858f0ba
Yuvaraja <paulyuvi02222@gmail.com>
2023-11-16 22:11:18 +0530

    Footer finish - Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/header/Header";
 import Home from "./components/home/home";
 import Skills from "./components/Skills/Skills";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -11,6 +12,7 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <main className="main">
+      <Header />
       <Home />
       <Skills />
       <Portfolio />
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "./footer.css";
+import { FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__container container social">
+        <div className="footer__socials">
+          <a href="" className="footer__social-link">
+            <FaTwitter />
+          </a>
+
+          <a href="" className="footer__social-link">
+            <FaLinkedin />
+          </a>
+
+          <a href="" className="footer__social-link">
+            <FaInstagram />
+          </a>
+        </div>
+
+        <p className="footer__copyright text-cs">
+          &copy; 2023 <span>Uv Arts</span>. All Rights Reserved
+        </p>
+
+        <p className="footer__copyright text-cs">
+          Developed by <span>Yuvaraja</span>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
